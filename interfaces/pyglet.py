5cacc73944ea95bc0e4acf42fb161e6e781da297
Antoine Rozo <antoine.rozo@gmail.com>
2016-01-16 20:45:52 +0100

    Simplifications

--- interfaces/pyglet.py
+++ interfaces/pyglet.py
@@ -1,12 +1,5 @@
 import pyglet
 
-window = pyglet.window.Window(width=800, height=600)
-keys = pyglet.window.key.KeyStateHandler()
-window.push_handlers(keys)
-@window.event
-def on_expose():
-    pass
-
 grounds_img = pyglet.image.load('res/sols.png')
 grounds_imggrid = pyglet.image.ImageGrid(grounds_img, 50, 12)
 grounds_texgrid = pyglet.image.TextureGrid(grounds_imggrid)
@@ -39,20 +32,27 @@ class _:
         if map is None:
             self.sprite = None
         else:
-            self.sprite = pyglet.sprite.Sprite(player_img, x=self.x*16, y=self.y*16, batch=map.batch, group=map.event_groups[self.z])
+            self.sprite = pyglet.sprite.Sprite(
+                player_img,
+                x=self.x*16, y=(self.y+self.z)*16,
+                batch=map.batch, group=map.event_groups[self.z])
 
+
+@engine.meta.register('engine.tile.Tile')
+class _:
+    img = grounds_texgrid[0, 0]
 
 @engine.meta.register('engine.tile.Grass')
 class _:
-    sprite = grounds_texgrid[49, 0]
+    img = grounds_texgrid[49, 0]
 
 @engine.meta.register('engine.tile.HighGrass')
 class _:
-    sprite = grounds_texgrid[48, 3]
+    img = grounds_texgrid[48, 3]
 
 @engine.meta.register('engine.tile.Teleport')
 class _:
-    sprite = grounds_texgrid[48, 4]
+    img = grounds_texgrid[48, 4]
 
 
 @engine.meta.register('engine.map.Map')
@@ -60,69 +60,85 @@ class _:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.batch = pyglet.graphics.Batch()
-        self.sprites = []
         self.tile_groups = [pyglet.graphics.OrderedGroup(2 * i) for i in range(self.levels)]
         self.event_groups = [pyglet.graphics.OrderedGroup(2 * i + 1) for i in range(self.levels)]
 
         for z, level in enumerate(self.tiles):
             for y, line in enumerate(level):
                 for x, tile in enumerate(line):
-                    if hasattr(tile, 'sprite'):
-                        self.sprites.append(
-                            pyglet.sprite.Sprite(
-                                tile.sprite,
-                                x=x*16, y=(y+z)*16,
-                                batch=self.batch, group=self.tile_groups[z])
-                        )
+                    tile.sprite = pyglet.sprite.Sprite(
+                        tile.img,
+                        x=x*16, y=(y+z)*16,
+                        batch=self.batch, group=self.tile_groups[z])
 
 
 import time
 class Clock:
-    def __init__(self):
+    def __init__(self, max=None):
         self.reset()
+        self.max = max
     def reset(self):
         self.time = time.time()
     @property
     def elapsed(self):
         return time.time() - self.time
+    @property
+    def finished(self):
+        if self.max is None:
+            return False
+        return self.elapsed > self.max
 
 @engine.meta.register('engine.game.Game')
 class _:
-    def run(self):
-        @window.event
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.window = pyglet.window.Window(width=800, height=600)
+        @self.window.event
+        def on_expose():
+            pass
+        @self.window.event
         def on_draw():
-            if self.player.map is None:
-                return
-            dx = window.width // 2 - self.player.sprite.x
-            dy = window.height // 2 - self.player.sprite.y
-            window.clear()
-            pyglet.gl.glTranslatef(dx, dy, 0)
-            self.player.map.batch.draw()
-            pyglet.gl.glTranslatef(-dx, -dy, 0)
+            self.draw()
 
-        TICK = 0.3
-        signals_clock = Clock()
-        keyboard_clock = Clock()
-        def update(dt):
-            if self.have_signals():
-                if signals_clock.elapsed >= TICK:
-                    self.handle_signals()
-                    signals_clock.reset()
-                return
-            if keyboard_clock.elapsed >= TICK:
-                dx, dy = 0, 0
-                if keys.get(pyglet.window.key.LEFT):
-                    dx = -1
-                if keys.get(pyglet.window.key.RIGHT):
-                    dx = 1
-                if keys.get(pyglet.window.key.UP):
-                    dy = 1
-                if keys.get(pyglet.window.key.DOWN):
-                    dy = -1
-                if dx or dy:
-                    self.player.walk(dx, dy)
-                    signals_clock.reset()
-                    keyboard_clock.reset()
-        pyglet.clock.schedule(update)
+        self.keys = pyglet.window.key.KeyStateHandler()
+        self.window.push_handlers(self.keys)
 
+        tick = 0.3
+        tick = 0.
+        self.signals_clock = Clock(tick)
+        self.keyboard_clock = Clock(tick)
+        pyglet.clock.schedule(self.update)
+
+    def draw(self):
+        if self.player.map is None:
+            return
+        dx = self.window.width // 2 - self.player.sprite.x
+        dy = self.window.height // 2 - self.player.sprite.y
+        self.window.clear()
+        pyglet.gl.glTranslatef(dx, dy, 0)
+        self.player.map.batch.draw()
+        pyglet.gl.glTranslatef(-dx, -dy, 0)
+
+    def update(self, _dt):
+        if self.have_signals():
+            if self.signals_clock.finished:
+                self.handle_signals()
+                self.signals_clock.reset()
+            return
+        if self.keyboard_clock.finished:
+            dx, dy = 0, 0
+            if self.keys.get(pyglet.window.key.LEFT):
+                dx = -1
+            elif self.keys.get(pyglet.window.key.RIGHT):
+                dx = 1
+            if self.keys.get(pyglet.window.key.UP):
+                dy = 1
+            elif self.keys.get(pyglet.window.key.DOWN):
+                dy = -1
+            if dx or dy:
+                self.player.walk(dx, dy)
+                self.signals_clock.reset()
+                self.keyboard_clock.reset()
+
+    def run(self):
         pyglet.app.run()
--- launch.py
+++ launch.py
@@ -7,13 +7,15 @@ from engine import *
 game = game.Game()
 
 pikachu = beast.BeastFamily('Pikachu', 'Electrik')
+carapuce = beast.BeastFamily('Carapuce', 'Eau')
 
 pikagroup = zone.WildGroup(pikachu, 10) # group of 10 pikachus
-pikazone = zone.Zone('grass', [pikagroup])
+caragroup = zone.WildGroup(carapuce, 4) # group of 4 carapuces
+zone = zone.Zone('grass', [pikagroup, caragroup])
 
 tile_chars = {
     '.': tile.Grass,
-    '*': lambda: tile.HighGrass(pikazone),
+    '*': lambda: tile.HighGrass(zone),
     '>': lambda: tile.Teleport((0,0), 'road'),
     '<': lambda: tile.Teleport((3,16), 'bourg'),
 }
@@ -46,7 +48,7 @@ bourg_tiles = [
     for level in bourg_tiles
 ]
 bourg_events = (character.Character(), character.Trainer(), object.Object())
-bourg_zones = [pikazone]
+bourg_zones = [zone]
 bourg = map.Map.from_tiles(bourg_tiles, bourg_events, bourg_zones)
 game.maps['bourg'] = bourg
 
@@ -82,7 +84,7 @@ road_tiles = [
     for level in road_tiles
 ]
 road_events = ()
-road_zones = [pikazone]
+road_zones = [zone]
 road = map.Map.from_tiles(road_tiles, road_events, road_zones)
 game.maps['road'] = road
 
