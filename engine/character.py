ee06edbd1baec2f0c3bb1a17ded06201f917f9aa
Antoine Rozo <antoine.rozo@gmail.com>
2016-01-20 08:29:30 +0100

    Boolean operations for moving characters

--- engine/character.py
+++ engine/character.py
@@ -14,18 +14,20 @@ class Character(Event):
         if map is None:
             map = self.map
         pos = x, y, z
-        if map.can_move(pos):
-            oldmap = self.map
-            old = self.position
-            self.map = map
-            self.position = pos
-            self.send(self.map.moved, oldmap, old, pos)
+        if not map.can_move(pos):
+            return False
+        oldmap = self.map
+        old = self.position
+        self.map = map
+        self.position = pos
+        self.send(self.map.moved, oldmap, old, pos)
+        return True
 
     def turn(self, dx, dy):
         self.direction = dx, dy
 
     def walk(self):
-        self.move(*self.map.walk_position(self.position, self.direction))
+        return self.move(*self.map.walk_position(self.position, self.direction))
 
     @property
     def direction(self):
