1dabf4b66b885e8c76569613cf20efd82a135e3a
Antoine Rozo <antoine.rozo@gmail.com>
2016-01-22 16:52:06 +0100

    Split game definitions

--- launch.py
+++ launch.py
@@ -5,29 +5,7 @@ DEBUG = True
 import interfaces.pyglet
 
 from engine import *
-
-class BourgChar(character.Character):
-    def __init__(self, *args, **kwargs):
-        character.Character.__init__(self, *args, **kwargs)
-        self.n = 0
-
-    def actioned(self, game, player, map, pos):
-        x, y, z = pos
-        dx, dy = player.x - x, player.y - y
-        self.turn(dx, dy)
-        dialog.Dialog('Hello', player, self)
-
-    def step(self, game):
-        self.n = (self.n + 1) % 5
-        if self.n:
-            return
-        if not self.walk():
-            dx, dy = self.direction
-            dc = dx + dy * 1j
-            dc *= 1j
-            dx, dy = int(dc.real), int(dc.imag)
-            self.turn(dx, dy)
-
+from game_defs import BourgChar
 
 game = game.Game()
 
